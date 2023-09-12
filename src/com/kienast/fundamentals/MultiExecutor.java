df451cbda02b5391ba51921d5859b345f414eaf2
LucasKienast <lucas.kienast@hotmail.de>
2023-09-12 13:32:50 +0100

    multi executor

---
+++ src/com/kienast/fundamentals/MultiExecutor.java
@@ -0,0 +1,34 @@
+package com.kienast.fundamentals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiExecutor {
+
+    private final List<Runnable> tasks;
+
+    public MultiExecutor(List<Runnable> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void executeAll() {
+        List<Thread> threads = new ArrayList<>(tasks.size());
+
+        for (Runnable task: tasks) {
+            Thread thread = new Thread(task);
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.out.println("Thread was interrupted!");
+            }
+        }
+    }
+}
